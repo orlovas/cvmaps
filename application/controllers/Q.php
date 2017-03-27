3d9b64893a0911c5fd4bfcd9448ec9c197deca75
Sergejus Orlovas <sergejus@orlovas.lt>
2017-03-27 21:35:33 +0300

    added gauges, user-editable salary and distance

--- application/controllers/Q.php
+++ application/controllers/Q.php
@@ -8,65 +8,49 @@ class Q extends CI_Controller {
         $this->load->helper('url_helper');
     }
 
-    public function m($qt = 0, $category_id = 0, $city_id = 0,$edu_id = NULL,$salary = NULL,$new = NULL,$premium = NULL, $work_time = NULL, $worker_type_id = NULL, $is_student = 0, $is_school = 0, $is_pensioneer = 0, $is_disabled = 0, $is_shift = 0, $no_exp = 0)
+    public function m()
     {
-        /*$page = $this->input->get('page', TRUE);
-		if(!isset($page)){
-			$page = 1;
-		}
-		$category_id = $this->input->get('category_id', TRUE);
+        $category_id = $this->input->get('category_id', TRUE);
 		$city_id = $this->input->get('city_id', TRUE);
 		$edu_id = $this->input->get('edu_id', TRUE);
-		$order_by = $this->input->get('order_by', TRUE);
+		$work_time_id = $this->input->get('work_time_id', TRUE);
 		$salary = $this->input->get('salary', TRUE);
-		$premium = $this->input->get('premium', TRUE);
-		$new = $this->input->get('new', TRUE);*/
-        if(strpos($salary,",")){
-            $salary = explode(",",$salary);
-        }
 
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode($this->queries->get_markers(
-            $qt,$category_id,$city_id,$edu_id,$salary,$new,$premium,$work_time,
-            $worker_type_id,$is_student,$is_school,$is_pensioneer,$is_disabled,$is_shift,$no_exp
+            $category_id,$city_id,$edu_id,$salary,$work_time_id
         )));
     }
 
-    public function j($page = NULL,$query_text = NULL,$order_by = NULL,$city_id = NULL,$category_id = NULL,$edu_id = NULL,$salary = NULL,$new = NULL,$premium = NULL, $work_time = NULL, $worker_type_id = NULL, $is_student = 0, $is_school = 0, $is_pensioneer = 0, $is_disabled = 0, $is_shift = 0, $no_exp = 0)
+    public function j()
     {
-        /*$page = $this->input->get('page', TRUE);
+        $page = $this->input->get('page', TRUE);
 		if(!isset($page)){
 			$page = 1;
 		}
 		$category_id = $this->input->get('category_id', TRUE);
 		$city_id = $this->input->get('city_id', TRUE);
 		$edu_id = $this->input->get('edu_id', TRUE);
+		$work_time_id = $this->input->get('work_time_id', TRUE);
 		$order_by = $this->input->get('order_by', TRUE);
 		$salary = $this->input->get('salary', TRUE);
-		$premium = $this->input->get('premium', TRUE);
-		$new = $this->input->get('new', TRUE);*/
-        if(strpos($salary,",")){
-            $salary = explode(",",$salary);
-        }
+
+
         if(isset($page)
-            && isset($query_text)
             && isset($order_by)
             && isset($city_id)
             && isset($category_id)
             && isset($edu_id)
-            && isset($salary)
-            && isset($new)
-            && isset($premium)) {
+            && isset($salary)) {
             $this->output->set_content_type('application/json');
             $this->output->set_output(json_encode($this->queries->get_jobs(
-                $page,$query_text,$order_by,$city_id,$category_id,$edu_id,$salary,$new,$premium,$work_time,
-                $worker_type_id,$is_student,$is_pensioneer,$is_disabled,$is_shift,$no_exp
+                $page,$order_by,$city_id,$category_id,$edu_id,$salary,$work_time_id
             )));
         }
 
     }
 
-    public function get_job($id)
+    /*public function get_job($id)
     {
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode($this->queries->get_job_by_id($id)));
@@ -83,26 +67,32 @@ class Q extends CI_Controller {
                 header("location:index.php");
             }
         }
-    }
+    }*/
 
-    public function get_jobs($ids)
+    public function get_jobs()
     {
+        $ids = $this->input->get('ids', TRUE);
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode($this->queries->get_jobs_by_ids($ids)));
     }
 
-    public function find_job($query_text = "")
+    /*public function find_job($query_text = "")
     {
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode($this->queries->find_job_by_title($query_text)));
-    }
+    }*/
 
-    public function init_param($page = NULL,$query_text = NULL,$order_by = NULL,$city_id = NULL,$category_id = NULL,$edu_id = NULL,$salary = NULL,$new = NULL,$premium = NULL, $work_time = NULL, $worker_type_id = NULL, $is_student = 0, $is_school = 0, $is_pensioneer = 0, $is_disabled = 0, $is_shift = 0, $no_exp = 0)
+    public function init_param()
     {
+        $category_id = $this->input->get('category_id', TRUE);
+		$city_id = $this->input->get('city_id', TRUE);
+		$edu_id = $this->input->get('edu_id', TRUE);
+		$work_time_id = $this->input->get('work_time_id', TRUE);
+		$salary = $this->input->get('salary', TRUE);
+
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode($this->queries->init_param(
-            $page,$query_text,$order_by,$city_id,$category_id,$edu_id,$salary,$new,$premium,$work_time,
-            $worker_type_id,$is_student,$is_school,$is_pensioneer,$is_disabled,$is_shift,$no_exp
+            $city_id,$category_id,$edu_id,$salary,$work_time_id
         )));
     }
 }
